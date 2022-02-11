979a8c4c79af0a595ce0336f1e92c201dbc41822
Syed Murtza <syed.murtza@tauspace.com>
2022-02-11 16:42:02 +0500

    Add subtract func

--- lib/gad.ex
+++ lib/gad.ex
@@ -36,4 +36,24 @@ defmodule GAD do
     n1 + n2
   end
 
+  @spec subtract(number(), number()) :: number()
+  @doc """
+
+    iex> GAD.subtract(5,2)
+    iex> 3
+
+    iex> GAD.subtract(1,"")
+    iex> "Please use numbers"
+
+  """
+  def subtract(n1, n2)
+
+  def subtract(n1, n2) when not is_number(n1) or not is_number(n2) do
+    "Please use numbers"
+  end
+
+  def subtract(n1, n2) when is_number(n1) and is_number(n2) do
+    n1 - n2
+  end
+
 end
