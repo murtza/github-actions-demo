dcbf10a25d5b3658592abb27492fa089d3783a3b
Syed Murtza <syed.murtza@tauspace.com>
2022-02-13 15:40:31 +0500

    Add min test for ssum func

--- test/gad_test.exs
+++ test/gad_test.exs
@@ -15,6 +15,10 @@ defmodule GADTest do
     assert GAD.sum(1, "2") == {:error, "Please use numbers"}
   end
 
+  test "test sum funcion failure min" do
+    assert GAD.sum(1, 0) == {:error, "Number must be greater than 0"}
+  end
+
   test "test subtract funcion" do
     assert GAD.subtract(5, 2) == {:ok, 3}
   end
